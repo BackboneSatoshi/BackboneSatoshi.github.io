b85c5db4d5de287af73584eda3252a7f7dc1002e
BackboneSatoshi <volvoxsatoshi@yahoo.co.jp>
2015-07-01 11:03:29 +0900

    remove img folder and image is loaded from robohash

--- app/js/collections/contacts.js
+++ app/js/collections/contacts.js
@@ -1,4 +1,54 @@
 SPA.Collections.Contacts = Backbone.Collection.extend({
   model: SPA.Models.Contact,
-  localStorage: new Backbone.LocalStorage('contacts')
+  initialize: function() {
+  	this._createSampleData();
+  },
+  localStorage: new Backbone.LocalStorage('contacts'),
+  _createSampleData: function() {
+    _.each([
+      {
+        id: 1,
+        name : 'Taketani Saki',
+        tel: '111-2222-3333',
+        email: 'Taketanisaki@backbone.com',
+        avatar: 'http://robohash.org/1?size=128x128'
+      },
+      {
+        id: 2,
+        name : 'Kimura Miki',
+        tel: '111-2234-3333',
+        email: 'kimuramiki@backbone.com',
+        avatar: 'http://robohash.org/2?size=128x128'
+      },
+      {
+        id: 3,
+        name : 'Gouda Takeshi',
+        tel: '918-1774-0199',
+        email: 'Goudatakeshi@backbone.com',
+        avatar: 'http://robohash.org/3?size=128x128'
+      },
+      {
+        id: 4,
+        name : 'Mikasa Kotone',
+        tel: '702-9289-5145',
+        email: 'mikasakotone@backbone.com',
+        avatar: 'http://robohash.org/4?size=128x128'
+      },
+      {
+        id: 5,
+        name : 'Fujimoto Satoshi',
+        tel: '318-2292-6700',
+        email: 'fujimotosatoshi@backbone.com',
+        avatar: 'http://robohash.org/5?size=128x128'
+      },
+      {
+        id: 6,
+        name : 'Robot Automation',
+        tel: '803-2557-9815',
+        email: 'robotautomation@jourrapide.com',
+        avatar: 'http://robohash.org/6?size=128x128'
+      }], function(contact) {
+        this.create(contact);
+      }, this);
+  }
 });
--- app/js/controller.js
+++ app/js/controller.js
@@ -3,18 +3,6 @@ SPA.Controller = Marionette.Controller.extend({
     this._router = options.router;
     this._contacts = options.contacts;
     this._proxy = options.proxy;
-    console.log(this._proxy);
-
-    // localStorageがからモデルを取得する場合はコメントを削除
-    // localStorageのモデルを削除する時はlocalStorage.clear();を
-    //　コソール画面で実行する
-    //this._contacts.fetch();
-
-    /*
-    if (this._contacts.isEmpty()) {
-      this._createSampleData();
-    }*/
-    this._createSampleData();
   },
 
   showContacts: function() { 
@@ -60,7 +48,7 @@ SPA.Controller = Marionette.Controller.extend({
     });
     // イベントリスニング
     this.listenTo(newContactForm, 'form:submitted', function(attrs) {
-      attrs.avatar = _.random(1, 15) + '.jpg';
+      attrs.avatar = 'http://robohash.org/' + _.random(1, 15) + '?size=140x140';
       this._contacts.create(attrs);
       this.showContacts();
     });
@@ -107,53 +95,5 @@ SPA.Controller = Marionette.Controller.extend({
     } else {
       this.showContacts();
     }
-  },
-
-  _createSampleData: function() {
-    _.each([
-      {
-        id: 1,
-        name : 'Terrence S. Hatfield',
-        tel: '651-603-1723',
-        email: 'TerrenceSHatfield@rhyta.com',
-        avatar: '1.jpg'
-      },
-      {
-        id: 2,
-        name : 'Chris M. Manning',
-        tel: '513-307-5859',
-        email: 'ChrisMManning@dayrep.com',
-        avatar: '2.jpg'
-      },
-      {
-        id: 3,
-        name : 'Ricky M. Digiacomo',
-        tel: '918-774-0199',
-        email: 'RickyMDigiacomo@teleworm.us',
-        avatar: '3.jpg'
-      },
-      {
-        id: 4,
-        name : 'Michael K. Bayne',
-        tel: '702-989-5145',
-        email: 'MichaelKBayne@rhyta.com',
-        avatar: '4.jpg'
-      },
-      {
-        id: 5,
-        name : 'John I. Wilson',
-        tel: '318-292-6700',
-        email: 'JohnIWilson@dayrep.com',
-        avatar: '5.jpg'
-      },
-      {
-        id: 6,
-        name : 'Rodolfo P. Robinett',
-        tel: '803-557-9815',
-        email: 'RodolfoPRobinett@jourrapide.com',
-        avatar: '6.jpg'
-      }], function(contact) {
-        this._contacts.create(contact);
-      }, this);
   }
 });
