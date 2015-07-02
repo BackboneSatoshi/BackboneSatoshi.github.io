7a9373bf4bd204112da503f0a37406fb155b8239
BackboneSatoshi <volvoxsatoshi@yahoo.co.jp>
2015-07-02 10:05:48 +0900

    update main.css

--- app/js/views/contact.js
+++ app/js/views/contact.js
@@ -1,6 +1,6 @@
 SPA.Views.Contact = Marionette.ItemView.extend({
   tagName: 'li',
-  className: 'media col-md-6 col-lg-4',
+  className: 'media col-sm-6 col-md-6 col-lg-4',
   template: '#tpl-contact',
   triggers: {
     'click .delete-contract': 'delete:clicked',
